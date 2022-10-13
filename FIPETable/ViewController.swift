bb29100deaa8e8a7632c3d15ed3359a92ff0d36a
Milena Alcantara <milena.lima.alcantara61@aluno.ifce.edu.br>
2022-10-13 11:35:13 -0300

    add gitignore

---
+++ FIPETable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FIPETable
+//
+//  Created by Milena Lima de Alcântara on 13/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FIPETableUITests/FIPETableUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FIPETableUITestsLaunchTests.swift
+//  FIPETableUITests
+//
+//  Created by Milena Lima de Alcântara on 13/10/22.
+//
+
+import XCTest
+
+final class FIPETableUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
